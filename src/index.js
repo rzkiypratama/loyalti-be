1e3d2e0f1fe45e028b6e5aca866cacd5d0eacca0
rzkiypratama <pratamabusiness@gmail.com>
2025-09-03 18:53:10 +0800

    fix: comment out unused webhook routes in index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import { Router } from "express";
 import publicRoutes from "./routes/public.routes.js";
-import webhookRoutes from "./routes/webhook-copy.routes.js";
+// import webhookRoutes from "./routes/webhook-copy.routes.js";
 
 const router = Router();
 
 router.use("/api", publicRoutes); // /api/public/*
-router.use("/webhooks", webhookRoutes); // /webhooks/*
+// router.use("/webhooks", webhookRoutes); // /webhooks/*
 
 export default router;
